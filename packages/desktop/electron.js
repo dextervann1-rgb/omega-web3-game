dc7c8faea9f8a77d20457da8012149c8ea617024
dextervann1-rgb <dextervann1@gmail.com>
2026-08-05 01:26:28 +0000

    feat: add electron desktop wrapper

---
+++ packages/desktop/electron.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow() {
+  const win = new BrowserWindow({ 
+    width: 1920, 
+    height: 1080,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  })
+  
+  // Using the Vercel URL as requested
+  win.loadURL('https://omega-web3-game.vercel.app') 
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
